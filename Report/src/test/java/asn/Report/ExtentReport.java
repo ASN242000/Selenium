2c3ba2cf20b5e3bc51431059e2dbef265a4cf843
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-03-14 19:16:55 +0530

    Add files via upload

--- Report/src/test/java/asn/Report/ExtentReport.java
+++ Report/src/test/java/asn/Report/ExtentReport.java
@@ -2,14 +2,40 @@ package asn.Report;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class ExtentReport {
+	ExtentReports extent;
+	@BeforeTest
+	public void config() {
+		String path= System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Anusha");
+		
+		
+	}
+	
 	@Test
 	public void initializeDemo() {
+		
+		ExtentTest test = extent.createTest("initialDemo");
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.google.com");
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result do not match");
+		extent.flush();
 	}
 }
