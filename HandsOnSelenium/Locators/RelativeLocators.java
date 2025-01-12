86e3c5fe404738224f91710441b2b4e7df0e879b
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-01-12 08:24:22 +0530

    Add files via upload

---
+++ HandsOnSelenium/Locators/RelativeLocators.java
@@ -0,0 +1,44 @@
+package Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.locators.RelativeLocator.RelativeBy;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class RelativeLocators {
+WebDriver driver;
+	
+	@BeforeTest
+	void setupClass() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\OneDrive\\Documents\\chromedriver-win64\\chromedriver.exe");
+		
+	}
+	
+	@BeforeMethod
+	void setup() {
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void test() {
+		
+		String url = "https://bonigarcia.dev/selenium-webdriver-java/web-form.html";
+		driver.get(url);
+		WebElement link = driver.findElement(By.linkText("Return to index"));
+		RelativeBy relativeBy = RelativeLocator.with(By.tagName("input"));
+		WebElement readOnly = driver.findElement(relativeBy.above(link));
+		Assert.assertEquals(readOnly.getDomAttribute("name"), "my-readonly");
+	}
+	
+	@AfterMethod
+	void teardown() {
+		driver.quit();
+	}
+}
